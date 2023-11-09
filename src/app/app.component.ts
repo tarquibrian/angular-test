751b07b6fc99dfa6ad144e3919cf903d247d0b4d
brian tarqui <tarquibrian@gmail.com>
2023-11-08 21:45:24 -0400

    configured styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'firstapp';
+  title = 'hola gente';
+
+  changeTitle() {
+    if (this.title === 'hola gente') {
+      this.title = 'adios gente';
+    } else {
+      this.title = 'hola gente';
+    }
+  }
 }
